8576c345576602f3ef568266ed22e3ae27185508
jungseoik <si.jung@pia.space>
2025-03-13 19:21:46 +0900

    [feat] tensorrt convert module add

--- assets/__init__.py
+++ assets/__init__.py
@@ -16,4 +16,20 @@ def download_required_files():
     except Exception as e:
         print(f"Error downloading required files: {e}")
 
+def download_required_files2():
+    """Initialize required files from Hugging Face Hub"""
+    try:
+        cache_dir = "assets/"
+        if not os.path.exists(os.path.join(cache_dir, "CLIP_EBC_nwpu_rmse.pth")):
+            hf_hub_download(
+                repo_id="PIA-SPACE-LAB/CLIP_EBC_nwpu_rmse",
+                filename="CLIP_EBC_nwpu_rmse.pth",
+                # cache_dir=cache_dir,
+                local_dir=cache_dir
+            )
+        print("Required files downloaded successfully")
+    except Exception as e:
+        print(f"Error downloading required files: {e}")
+
 download_required_files()
+download_required_files2()
